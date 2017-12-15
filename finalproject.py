0b55b07ab8773b7c8da45f184af649ee561ef7d1
Catie <catieo@umich.edu>
2017-12-14 19:21:01 -0600

    Visualizations complete

--- finalproject.py
+++ finalproject.py
@@ -2,16 +2,21 @@ import requests
 import json
 import yelp
 import sqlite3
+import plotly
+import plotly.plotly as py 
+from plotly.graph_objs import * 
 
 #secret values to authenticate to the Yelp Fusion API 
 client_id = yelp.client_id
 client_secret = yelp.client_secret
 access_token = yelp.access_token
 
+#secret values for Plotly 
+plotly.tools.set_credentials_file(username=yelp.plotly_username, api_key=yelp.plotly_api_key)
+
 #set up authentication to Yelp
 base_url = "https://api.yelp.com"
 search_api_path = "/v3/businesses/search"
-#COME BACK TO THIS AND REFORMAT STRING WITH EACH BUSINESS WHEN YOU MAKE THE REQUEST
 reviews_api_path = "/v3/businesses/{}/reviews"
 
 #caching pattern for search_results_cache
@@ -110,24 +115,86 @@ for business in results2["businesses"]:
 		rtup = business["id"], review["time_created"], review["rating"], review["text"], review["user"]["name"]
 		cur.execute('INSERT INTO Reviews (bus_id, time_posted, ind_rating, review_text, user_posted) VALUES (?, ?, ?, ?, ?)', rtup)
 
-#join Business and Review tables according to bus_id key 
-#save in a list of tuples for now, maybe make another table? 
+#join Business and Review tables according to bus_id key  
 joined_data = []
-for row in cur.execute('SELECT * FROM Businesses JOIN Reviews ON Businesses.id = Reviews.bus_id'):
+for row in cur.execute('SELECT Businesses.id, Reviews.time_posted, Businesses.rating FROM Businesses JOIN Reviews ON Businesses.id = Reviews.bus_id'):
 	joined_data.append(row)
-print(joined_data[0], joined_data[1], joined_data[2], joined_data[3])
 
 conn.commit()
 cur.close()
 
+#Plotly visualization
+#create lists of reviews by hour of the day 
+possible_hours = []
+num_3_per_hour = []
+num_35_per_hour = []
+num_4_per_hour = []
+num_45_per_hour = []
+num_5_per_hour = []
+temp3 = {}
+temp35 = {}
+temp4 = {}
+temp45 = {}
+temp5 = {}
 
+for tup in joined_data:
+	x = tup[1][11:13]
+	if tup[2] == 3.0:
+		if x not in temp3:
+			temp3[x] = 1
+		else:
+			temp3[x] += 1 
+		if x not in possible_hours:
+			possible_hours.append(x)
+	if tup[2] == 3.5:
+		if x not in temp35:
+			temp35[x] = 1
+		else:
+			temp35[x] += 1 
+		if x not in possible_hours:
+			possible_hours.append(x)
+	if tup[2] == 4.0:
+		if x not in temp4:
+			temp4[x] = 1
+		else:
+			temp4[x] += 1 
+		if x not in possible_hours:
+			possible_hours.append(x)
+	if tup[2] == 4.5:
+		if x not in temp45:
+			temp45[x] = 1
+		else:
+			temp45[x] += 1 
+		if x not in possible_hours:
+			possible_hours.append(x)
+	if tup[2] == 5.0:
+		if x not in temp5:
+			temp5[x] = 1
+		else:
+			temp5[x] += 1 
+		if x not in possible_hours:
+			possible_hours.append(x)
+for x in temp3:
+	num_3_per_hour.append(temp3[x])
+for x in temp35:
+	num_35_per_hour.append(temp35[x])
+for x in temp4:
+	num_4_per_hour.append(temp4[x])
+for x in temp45:
+	num_45_per_hour.append(temp45[x])
+for x in temp5:
+	num_5_per_hour.append(temp5[x])
 
-	
-
-
-
-
-
+#draw graph
+trace3 = graph_objs.Bar(x=possible_hours, y=num_3_per_hour, name='Rating 3.0')
+trace35 = graph_objs.Bar(x=possible_hours, y=num_35_per_hour, name='Rating 3.5')
+trace4 = graph_objs.Bar(x=possible_hours, y=num_4_per_hour, name='Rating 4.0')
+trace45 = graph_objs.Bar(x=possible_hours, y=num_45_per_hour, name='Rating 4.5')
+trace5 = graph_objs.Bar(x=possible_hours, y=num_5_per_hour, name='Rating 5.0')
+data = [trace3,trace35,trace4,trace45,trace5]
+layout = graph_objs.Layout(barmode = 'group', title='Different Ratings Given Per Hour')
+fig = graph_objs.Figure(data=data, layout=layout)
+py.plot(fig, filename='ratings_per_hour')
 
 
 
